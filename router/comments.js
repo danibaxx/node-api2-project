d5c12cbefda0240a56969118603e80ab35c0c070
Danielle B <danibaxx@hotmail.com>
2019-12-14 21:38:58 -0700

    added GET req for comments, working; POST req for comments, working

--- router/comments.js
+++ router/comments.js
@@ -8,6 +8,7 @@ const router = express.Router({
 // has all /comments endpoints
 router.get('/', (req, res) => {
   const id = req.params.id;
+
   db.findPostComments(id)
     .then(data => {
       res.status(200).json(data)
@@ -19,21 +20,51 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:commentId', (req, res) => {
+  db.findCommentById(req.params.id, req.params.commentId)
+  .then(data => {
+    if (data) {
+      res.status(200).json(200)
+    } else {
+      res.status(400).json({
+        message: 'The comment with the specified ID does not exist.'
+      })
+    }
+  })
+  .catch(error => {
+    res.status(500).json({
+      error: 'The comments information could not be retrieved.'
+    })
+  })
+})
+
 // POST req by id w/ comments
-// refactor this one!
-router.post('/:id', (req, res) => {
-  db.findById(req.params.id)
-    .then(post => {
-      if (post) {
-        res.json(post)
-      } else if (!post) { 
-        res.status(404).json({
-          message: 'The post with the specified ID does not exist.'
+router.post('/', (req, res) => {
+  const id = req.params.id;
+  const newComment = {
+    text: req.body.text,
+    post_id: req.params.id
+  };
+  
+  if (!req.body.text) {
+    res.status(400).json({
+      errorMessage: 'Please provide text for comment.'
+    })
+  }
+
+  db.findById(id)
+    .then(data => {
+      if (!data) {
+        return res.status(404).json({
+          message: 'The comment with this ID does not exist.'
         })
-      } else {
-        res.status(400).json({
-          errorMessage: 'Please provide text for the comment.'
-        })
+      }
+    })
+
+  db.insertComment(newComment)
+    .then(comment => {
+      if (comment) {
+        res.status(200).json(comment)
       }
     })
     .catch(error => {
